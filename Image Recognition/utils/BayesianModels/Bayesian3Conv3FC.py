7cac422c7795d1c84d63a2cf0b13f8c4f86f194c
EvenStrangest <EvenStrangest@memtor.org.il>
2019-12-28 23:40:27 +0200

    trying to fix silly bug

--- Image Recognition/utils/BayesianModels/Bayesian3Conv3FC.py
+++ Image Recognition/utils/BayesianModels/Bayesian3Conv3FC.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import math
 from utils.BBBlayers import BBBConv2d, BBBLinearFactorial, FlattenLayer
 
 class BBB3Conv3FC(nn.Module):
@@ -9,26 +10,31 @@ class BBB3Conv3FC(nn.Module):
     """
     def __init__(self, outputs, inputs):
         super(BBB3Conv3FC, self).__init__()
-        self.conv1 = BBBConv2d(inputs, 32, 5, stride=1, padding=2)
+
+        self.q_logvar_init = 0.05
+        self.p_logvar_init = math.log(0.05)
+
+        self.conv1 = BBBConv2d(self.q_logvar_init, self.p_logvar_init, inputs, 32, 5, stride=1, padding=2)
         self.soft1 = nn.Softplus()
         self.pool1 = nn.MaxPool2d(kernel_size=3, stride=2)
 
-        self.conv2 = BBBConv2d(32, 64, 5, stride=1, padding=2)
+        self.conv2 = BBBConv2d(self.q_logvar_init, self.p_logvar_init, 32, 64, 5, stride=1, padding=2)
         self.soft2 = nn.Softplus()
         self.pool2 = nn.MaxPool2d(kernel_size=3, stride=2)
 
-        self.conv3 = BBBConv2d(64, 128, 5, stride=1, padding=1)
+        self.conv3 = BBBConv2d(self.q_logvar_init, self.p_logvar_init, 64, 128, 5, stride=1, padding=1)
         self.soft3 = nn.Softplus()
         self.pool3 = nn.MaxPool2d(kernel_size=3, stride=2)
 
         self.flatten = FlattenLayer(2 * 2 * 128)
-        self.fc1 = BBBLinearFactorial(2 * 2 * 128, 1000)
+
+        self.fc1 = BBBLinearFactorial(self.q_logvar_init, self.p_logvar_init, 2 * 2 * 128, 1000)
         self.soft5 = nn.Softplus()
 
-        self.fc2 = BBBLinearFactorial(1000, 1000)
+        self.fc2 = BBBLinearFactorial(self.q_logvar_init, self.p_logvar_init, 1000, 1000)
         self.soft6 = nn.Softplus()
 
-        self.fc3 = BBBLinearFactorial(1000, outputs)
+        self.fc3 = BBBLinearFactorial(self.q_logvar_init, self.p_logvar_init, 1000, outputs)
 
         layers = [self.conv1, self.soft1, self.pool1, self.conv2, self.soft2, self.pool2,
                   self.conv3, self.soft3, self.pool3, self.flatten, self.fc1, self.soft5,
--- Image Recognition/utils/BayesianModels/BayesianLeNet.py
+++ Image Recognition/utils/BayesianModels/BayesianLeNet.py
@@ -9,22 +9,27 @@ class BBBLeNet(nn.Module):
 
     def __init__(self, outputs, inputs):
         super(BBBLeNet, self).__init__()
-        self.conv1 = BBBConv2d(inputs,6, 5, stride=1)
+
+        self.q_logvar_init = 0.05
+        self.p_logvar_init = math.log(0.05)
+
+        self.conv1 = BBBConv2d(self.q_logvar_init, self.p_logvar_init, inputs, 6, 5, stride=1)
         self.soft1 = nn.Softplus()
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.conv2 = BBBConv2d(6, 16, 5, stride=1)
+        self.conv2 = BBBConv2d(self.q_logvar_init, self.p_logvar_init, 6, 16, 5, stride=1)
         self.soft2 = nn.Softplus()
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.flatten = FlattenLayer(5 * 5 * 16)
-        self.fc1 = BBBLinearFactorial(5 * 5 * 16, 120)
+
+        self.fc1 = BBBLinearFactorial(self.q_logvar_init, self.p_logvar_init, 5 * 5 * 16, 120)
         self.soft3 = nn.Softplus()
 
-        self.fc2 = BBBLinearFactorial(120, 84)
+        self.fc2 = BBBLinearFactorial(self.q_logvar_init, self.p_logvar_init, 120, 84)
         self.soft4 = nn.Softplus()
 
-        self.fc3 = BBBLinearFactorial(84, outputs)
+        self.fc3 = BBBLinearFactorial(self.q_logvar_init, self.p_logvar_init, 84, outputs)
 
         layers = [self.conv1, self.soft1, self.pool1, self.conv2, self.soft2, self.pool2,
                   self.flatten, self.fc1, self.soft3, self.fc2, self.soft4, self.fc3]
