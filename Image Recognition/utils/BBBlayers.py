4b9f863b5314b3d6113ab5c80ee259f1899077cf
EvenStrangest <EvenStrangest@memtor.org.il>
2019-12-28 23:18:21 +0200

    introduce Flatten

--- Image Recognition/utils/BBBlayers.py
+++ Image Recognition/utils/BBBlayers.py
@@ -81,3 +81,14 @@ class BBBLinearFactorial(nn.Module):
         out = F.linear(input, weight, bias)
         kl = kl_.sum() 
         return out, kl
+
+
+class FlattenLayer(nn.Module):
+
+    def __init__(self, num_features):
+        super(FlattenLayer, self).__init__()
+        self.num_features = num_features
+
+    def forward(self, x):
+        return x.view(-1, self.num_features)
+
